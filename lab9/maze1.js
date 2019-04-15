0cbcd1cbedaeb8d9e3df395e08e8cfe86ab6e481
selomieT <selomie.mteshome@gmail.com>
2019-04-15 12:54:33 -0500

    document updated

---
+++ lab9/maze1.js
@@ -0,0 +1,38 @@
+"use strict";
+$(document).ready(function(){
+    var touch = false;
+
+    $(".boundary:not(.example)").mouseover( function(){
+        if(touch){
+            touch = false;
+            $(".boundary:not(.example)").addClass("youlose");
+            $("#status").text("Sorry you lost :[");
+        }
+    });
+
+    $("#end").mouseover(function(){
+        if(touch){
+            $("#status").text("You win! :]");
+            touch = false;
+        } else if(touch){
+            $("#status").text("Sorry you lost :[");
+        }
+    });
+
+    $("#start").click(function(){
+        if(!touch){
+            touch = true;
+            $(".boundary:not(.example)").removeClass("youlose");
+            $("#status").text("Click the \"S\" to begin.");
+        }
+    });
+
+    $("#maze").mouseleave(function(){
+        if(touch){
+            touch = false;
+            $(".boundary:not(.example)").addClass("youlose");
+            $("#status").text("Sorry you lost :[");
+        }
+    });
+
+});
